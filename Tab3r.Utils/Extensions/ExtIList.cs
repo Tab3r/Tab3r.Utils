7108d2f0c1065baf42c44f527f7e88845c602498
David Tabernero <david.tabernero@gmail.com>
2012-08-02 21:07:55 +0200

    - First commit

---
+++ Tab3r.Utils/Extensions/ExtIList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// For Serialization
+using System.Runtime.Serialization;
+
+namespace Tab3r.Utils.Extensions
+{
+    /// <summary>
+    /// All extensions for IList
+    /// </summary>
+    public static class ExtIList
+    {
+        /// <summary>
+        /// Clone a IClonable objects list
+        /// </summary>
+        /// <typeparam name="T">Objects type</typeparam>
+        /// <param name="listToClone">List</param>
+        /// <returns>Cloned list</returns>
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+
+        /// <summary>
+        /// Clone a ISerializable objects list
+        /// </summary>
+        /// <typeparam name="T">Objects type</typeparam>
+        /// <param name="listToDeepClone">List</param>
+        /// <returns>Cloned list</returns>
+        public static IList<T> CloneDeep<T>(this IList<T> listToDeepClone) where T : ISerializable
+        {
+            return listToDeepClone.Select(item => (T)item.DeepClone()).ToList();
+        }
+
+        /// <summary>
+        /// Create a string from objects list
+        /// </summary>
+        /// <typeparam name="T">Objects type</typeparam>
+        /// <param name="obj">List</param>
+        /// <param name="delimiter">Delimiters to add each object together</param>
+        /// <returns>String</returns>
+        public static string AsDelimited<T>(this List<T> obj, string delimiter)
+        {
+            List<string> items = new List<string>();
+            foreach (T data in obj)
+            {
+                items.Add(data.ToString());
+            }
+            return String.Join(delimiter, items.ToArray());
+        }
+    }
+}
---
+++ Tab3r.Utils/Extensions/ExtObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+// For serialization
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Tab3r.Utils.Extensions
+{
+    public static class ExtObject
+    {
+        /// <summary>
+        /// Deep clone like Java
+        /// </summary>
+        /// <remarks>Very useful for DataSet, for example</remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static T DeepClone<T>(this T a) where T : ISerializable
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, a);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
