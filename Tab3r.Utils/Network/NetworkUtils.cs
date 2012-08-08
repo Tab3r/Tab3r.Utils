328ef282813cac03df74072c9f9d3706141c2dad
David Tabernero <david.tabernero@gmail.com>
2012-08-08 14:59:10 +0200

    - Added more utilities for fast developing :)

---
+++ Tab3r.Utils/Network/NetworkUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.NetworkInformation;
+
+namespace Tab3r.Utils.Network
+{
+    public static class NetworkUtils
+    {
+        /// <summary>
+        /// Make a simple ping to host
+        /// </summary>
+        /// <param name="host">
+        /// <para>host.</para>
+        /// <para>Could be a hostname or ip address</para>
+        /// </param>
+        /// <returns>If ping was done correctly</returns>
+        public static bool SimplePing(string host)
+        {
+            bool ok = true;
+            Ping pingSender = new Ping();
+            try
+            {
+                PingReply reply = pingSender.Send(host);
+                if (reply.Status == IPStatus.Success)
+                {
+
+#if DEBUG
+                    Console.WriteLine("Address: {0}", reply.Address.ToString());
+                    Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                    Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+#endif
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine(reply.Status);
+                    ok = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                ok = false;
+            }
+            return ok;
+        }
+
+
+    }
+}
---
+++ Tab3r.Utils/myResxFile/myResxFileBMPExtractor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Resources;
+using System.Collections;
+using System.Drawing;
+
+namespace Tab3r.Utils.myResxFile
+{
+    public static class myResxFileBMPExtractor
+    {
+
+        public void getBMPFiles(string resxFile)
+        {
+            // Create a Resource Reader
+            ResXResourceReader rsxr = new ResXResourceReader(resxFile);
+
+            // Create a enumerator
+            IDictionaryEnumerator id = rsxr.GetEnumerator();
+
+            int i = 0;
+            foreach (DictionaryEntry d in rsxr)
+            {
+
+                Bitmap b = d.Value as Bitmap;
+
+                if (b != null)
+                {
+                    b.Save(resxFile + "__" + i.ToString() + ".bmp");
+                    i++;
+                }
+
+            }
+
+            //Close the reader.
+            rsxr.Close();
+        }
+
+    }
+}
